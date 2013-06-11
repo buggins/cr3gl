5b7ceb0f98a1ef478024b76a2933ec3c2eb29c3c
Vadim Lopatin <buggins@fromru.com>
2013-06-11 08:51:16 +0400

    GamePlay template project

---
+++ cr3gp/src/CR3Main.h
@@ -0,0 +1,62 @@
+#ifndef CR3Main_H_
+#define CR3Main_H_
+
+#include "gameplay.h"
+
+using namespace gameplay;
+
+/**
+ * Main game class.
+ */
+class CR3Main: public Game
+{
+public:
+
+    /**
+     * Constructor.
+     */
+    CR3Main();
+
+    /**
+     * @see Game::keyEvent
+     */
+	void keyEvent(Keyboard::KeyEvent evt, int key);
+	
+    /**
+     * @see Game::touchEvent
+     */
+    void touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex);
+
+protected:
+
+    /**
+     * @see Game::initialize
+     */
+    void initialize();
+
+    /**
+     * @see Game::finalize
+     */
+    void finalize();
+
+    /**
+     * @see Game::update
+     */
+    void update(float elapsedTime);
+
+    /**
+     * @see Game::render
+     */
+    void render(float elapsedTime);
+
+private:
+
+    /**
+     * Draws the scene each frame.
+     */
+    bool drawScene(Node* node);
+
+    Scene* _scene;
+};
+
+#endif
