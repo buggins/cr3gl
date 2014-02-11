a3a12e6c552ec1f4a7d1e377868f6d82e96c50d3
Vadim Lopatin <buggins@fromru.com>
2014-02-11 23:51:04 +0400

    download manager - download is working

--- cr3android/src/org/coolreader/newui/DownloadManager.java
+++ cr3android/src/org/coolreader/newui/DownloadManager.java
@@ -1,5 +1,14 @@
 package org.coolreader.newui;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.ByteBuffer;
 import java.util.LinkedList;
 
 public class DownloadManager {
@@ -15,7 +24,7 @@ public class DownloadManager {
 	static int nextTaskId = 1;
 	private DownloadManagerCallback callback;
 	
-	private static class Task {
+	private class Task {
 		final int id;
 		final String url;
 		final String method;
@@ -23,6 +32,7 @@ public class DownloadManager {
 		final String password;
 		final String saveAs;
 		boolean cancelled;
+
 		Task(String url, String method, String login, String password, String saveAs) {
 			synchronized(DownloadManager.class) {
 				id = nextTaskId++;
@@ -33,11 +43,88 @@ public class DownloadManager {
 			this.password = password;
 			this.saveAs = saveAs;
 		}
+		
+		private void error(int code, String message) {
+			callback.onDownloadResult(id, url, code, message, "", 0, null);
+		}
+		
 		public void execute() {
 			if (cancelled)
 				return;
 			log.d("executing download for " + url);
+	        InputStream input = null;
+	        OutputStream output = null;
+	        HttpURLConnection connection = null;
+	        try {
+	            URL url = new URL(this.url);
+	            connection = (HttpURLConnection) url.openConnection();
+	            connection.connect();
+
+	            // expect HTTP 200 OK, so we don't mistakenly save error report
+	            // instead of the file
+	            int code = connection.getResponseCode();
+	            if (code != HttpURLConnection.HTTP_OK) {
+	            	error(code, "Server returned HTTP " + code
+	                        + " " + connection.getResponseMessage());
+	            	return;
+	            }
+
+	            // this will be useful to display download percentage
+	            // might be -1: server did not report the length
+	            int fileLength = connection.getContentLength();
+	            String mimeType = connection.getContentType();
+
+	            // download the file
+	            input = connection.getInputStream();
+	            ByteArrayOutputStream buf = null;
+	            if (saveAs != null && saveAs.length() > 0) {
+	            	output = new FileOutputStream(new File(saveAs));
+	            } else {
+	            	buf = new ByteArrayOutputStream();
+	            	output = buf;
+	            }
+
+	            byte data[] = new byte[4096];
+	            long total = 0;
+	            int count;
+	            while ((count = input.read(data)) != -1) {
+	                // allow canceling with back button
+	                if (cancelled) {
+	                    input.close();
+	                    error(1, "Cancelled");
+	                    break;
+	                }
+	                total += count;
+	                // publishing the progress....
+	                if (fileLength > 0) {// only if total length is known
+	                	callback.onDownloadProgress(id, this.url, code, "", mimeType, (int)total, fileLength);
+	                }
+	                output.write(data, 0, count);
+	            }
+	            if (!cancelled) {
+	            	byte[] res = null;
+	            	if (buf != null) {
+	            		res = buf.toByteArray();
+	            	}
+	            	callback.onDownloadResult(id, this.url, 0, "OK", mimeType, (int)total, res);
+	            }
+	        } catch (Exception e) {
+            	error(2, "Error while downloading: " + e.getMessage());
+            	return;
+	        } finally {
+	            try {
+	                if (output != null)
+	                    output.close();
+	                if (input != null)
+	                    input.close();
+	            } catch (IOException ignored) {
+	            }
+
+	            if (connection != null)
+	                connection.disconnect();
+	        }
 		}
+		
 		public void cancel() {
 			cancelled = true;
 		}
