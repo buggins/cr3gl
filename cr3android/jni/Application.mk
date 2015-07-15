65b90a46b9b6f146b676a5cc7bb8d96a6c3be11b
Vadim Lopatin <coolreader.org@gmail.com>
2015-07-15 15:47:07 +0300

    version

--- cr3android/jni/Application.mk
+++ cr3android/jni/Application.mk
@@ -1,7 +1,14 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_OPTIM := release
 #APP_OPTIM := debug
-APP_ABI := armeabi-v7a
+
+#APP_ABI := armeabi 
+#APP_ABI := armeabi-v7a
+#APP_ABI := mips 
+#APP_ABI := x86
+#APP_ABI := x86_64
+APP_ABI := arm64-v8a
+
 #APP_ABI := armeabi-v7a x86 mips armeabi
 #APP_ABI := x86_64 arm64-v8a
 APP_PLATFORM := android-8
